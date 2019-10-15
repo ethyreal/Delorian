adef1fc00d9ebd71f170799d6a24d49b09b78393
George Webster <george@ethyreal.com>
2019-10-15 16:38:06 -0700

    Initial Commit

---
+++ Delorian/Delorian.h
@@ -0,0 +1,19 @@
+//
+//  Delorian.h
+//  Delorian
+//
+//  Created by George Webster on 10/15/19.
+//  Copyright © 2019 George Webster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Delorian.
+FOUNDATION_EXPORT double DelorianVersionNumber;
+
+//! Project version string for Delorian.
+FOUNDATION_EXPORT const unsigned char DelorianVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Delorian/PublicHeader.h>
+
+
